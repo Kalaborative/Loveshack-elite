90ecb8dd31701cc1601e11bdb833e80b41f4483a
davisma <davisma@gmx.com>
2017-07-26 16:53:29 -0400

    app requirements

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+#Import the necessary modules.
 from flask import Flask, render_template
+from os import environ
 
+# Instantiate an 'app' object.
 app = Flask(__name__)
 
+# Assign a decorator to the default URL.
 @app.route("/")
-def index():
+def index(): # define the default URL as the index.
     return render_template("index.html")
 
+# run the app.
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    port = int(environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
